c40ccc8930a2afc35bd8e6043be53b4dd1fde497
Lucas Bledsoe <lbledsoe34@gmail.com>
2023-07-28 13:51:49 -0400

    app-routing

--- angular-application/src/app/app-routing.module.ts
+++ angular-application/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { EmptyTableComponent } from './empty-table/empty-table.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'empty', pathMatch: 'full' },
 
   { path: 'home', component: AppComponent },
+  { path: 'empty', component: EmptyTableComponent }
 ];
 
 @NgModule({
