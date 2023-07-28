d225f1ef622253e390179e887f2bca99d85569b5
Lucas Bledsoe <lbledsoe34@gmail.com>
2023-07-28 09:16:44 -0400

    testing

---
+++ angular-application/src/app/empty-table/empty-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-table',
+  templateUrl: './empty-table.component.html',
+  styleUrls: ['./empty-table.component.css']
+})
+export class EmptyTableComponent {
+
+}
